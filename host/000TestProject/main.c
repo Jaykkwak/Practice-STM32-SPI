6644b781f472ac7895c5bbac39868367e26556ca
Sungjea Kwak <tjdtjdwo@gmail.com>
2024-03-23 18:11:28 -0500

    Init git and filies

---
+++ host/000TestProject/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: Feb 27, 2024
+ *      Author: me
+ */
+
+
+#include<stdio.h>
+
+int main(void){
+	printf("hello world\n");
+
+	return 0;
+}
---
+++ target/stm32f4xx_drivers/Debug/Src/main.d
@@ -0,0 +1,7 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/tjdtj/STM32CubeIDE/workspace_1.14.1/target/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h \
+ C:/Users/tjdtj/STM32CubeIDE/workspace_1.14.1/target/stm32f4xx_drivers/drivers/Inc/stm32f4xx_gpio_driver.h \
+ C:/Users/tjdtj/STM32CubeIDE/workspace_1.14.1/target/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
+C:/Users/tjdtj/STM32CubeIDE/workspace_1.14.1/target/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
+C:/Users/tjdtj/STM32CubeIDE/workspace_1.14.1/target/stm32f4xx_drivers/drivers/Inc/stm32f4xx_gpio_driver.h:
+C:/Users/tjdtj/STM32CubeIDE/workspace_1.14.1/target/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
